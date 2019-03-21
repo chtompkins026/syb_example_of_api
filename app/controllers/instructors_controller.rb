e82a5fa7e2952e886ecfce1c0718b48c62bdcc73
chtompkins026 <chtompkins026@gmail.com>
2019-03-21 16:04:19 -0400

    Created application controller

---
+++ app/controllers/instructors_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::InstructorsController < ApplicationController
+  def index
+    render json: Instructor.all
+  end
+
+  def create
+    instructor = Instructor.create(instructor_params)
+    render json: Instructor
+  end
+
+  def destroy
+    Instructor.destroy(params[:id])
+  end
+
+  def update
+    instructor = Instructor.find(params[:id])
+    instructor.update_attributes(instructor_params)
+    render json: Instructor
+  end
+
+  private
+
+  def instructor_params
+    params.require(:instructor).permit(:id, :name, :email,:bio, :admin)
+  end
+end
